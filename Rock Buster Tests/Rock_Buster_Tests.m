e0ea392ca07aca21c1d1630c9d804a8c3dbc4136
Josh Wisenbaker <joshwisenbaker@Joshs-MacBook-Pro.local>
2014-02-04 11:46:39 -0500

    Getting new tests working

---
+++ Rock Buster Tests/Rock_Buster_Tests.m
@@ -0,0 +1,34 @@
+//
+//  Rock_Buster_Tests.m
+//  Rock Buster Tests
+//
+//  Created by Josh Wisenbaker on 2/4/14.
+//  Copyright (c) 2014 Me. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Rock_Buster_Tests : XCTestCase
+
+@end
+
+@implementation Rock_Buster_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+//    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
